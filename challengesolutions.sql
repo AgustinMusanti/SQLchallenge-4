02e8f9c787e7baeee97581445958d4815ee983ca
Agustin Musanti <138232526+AgustinMusanti@users.noreply.github.com>
2025-02-15 21:20:38 -0300

    >>

--- challengesolutions.sql
+++ challengesolutions.sql
@@ -194,3 +194,61 @@ ORDER BY calificacion_promedio DESC;
 
 
 ## 15- Calcula el ingreso total generado por las suscripciones en los últimos 3 meses.
+
+SELECT SUM(p.monto) AS ingreso_total
+FROM   pagos p
+WHERE  p.fecha_pago >= DATE_SUB(CURDATE(), INTERVAL 3 MONTH);
+
+
+## 16- Identifica el usuario que ha dejado más reseñas en la plataforma.
+
+SELECT u.usuario_id, 
+       u.nombre, 
+COUNT(c.calificacion_id) AS cantidad_reseñas
+FROM calificaciones c
+JOIN usuarios u ON c.usuario_id = u.usuario_id
+GROUP BY u.usuario_id, u.nombre
+ORDER BY cantidad_reseñas DESC
+LIMIT 1;
+
+
+## 17- Encuentra los usuarios inactivos
+
+SELECT usuario_id, nombre 
+FROM   usuarios 
+WHERE  estado = 'Inactivo';
+
+
+## 18- Lista las películas y series que no han sido vistas por ningún usuario.
+    
+SELECT c.contenido_id, c.titulo, c.tipo 
+FROM   contenido c
+LEFT JOIN visualizaciones v ON c.contenido_id = v.contenido_id
+WHERE v.contenido_id IS NULL;
+
+
+## 19- Obtén la cantidad de usuarios que tienen una suscripción activa en cada tipo de plan.
+
+SELECT u.tipo_suscripcion, COUNT(u.usuario_id) AS cantidad_usuarios
+FROM   usuarios u
+WHERE  u.estado = 'Activo'
+GROUP BY u.tipo_suscripcion
+ORDER BY cantidad_usuarios DESC;
+
+
+## 20- Crea una consulta que prediga cuándo un usuario tendrá que renovar su suscripción basándose en su fecha
+       de pago anterior.
+
+
+SELECT usuario_id, 
+       fecha_registro AS ultima_pago, 
+       CASE 
+           WHEN tipo_suscripcion = 'Mensual' THEN DATE_ADD(fecha_registro, INTERVAL 1 MONTH)
+           WHEN tipo_suscripcion = 'Anual' THEN DATE_ADD(fecha_registro, INTERVAL 1 YEAR)
+           WHEN tipo_suscripcion = 'Premium Familiar' THEN DATE_ADD(fecha_registro, INTERVAL 1 MONTH)
+           WHEN tipo_suscripcion = 'Gratuito con Ads' THEN NULL
+       END AS fecha_renovacion
+FROM usuarios
+WHERE estado = 'Activo';
+
+
